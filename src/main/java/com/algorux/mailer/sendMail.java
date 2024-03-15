6b24bb05f1fe32e331b1d15ddc97cfad75110692
Alejandro González <algorux@gmail.com>
2024-03-15 10:57:52 -0600

    Se agregan opciones para envio de correos arbitrarios

--- src/main/java/com/algorux/mailer/sendMail.java
+++ src/main/java/com/algorux/mailer/sendMail.java
@@ -1,4 +1,7 @@
 package com.algorux.mailer;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.AddressException;
+import jakarta.mail.internet.InternetAddress;
 import org.apache.catalina.core.ApplicationContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
+import java.util.regex.Pattern;
 
 @RestController
 public class sendMail extends EmailService {
@@ -26,18 +30,58 @@ public class sendMail extends EmailService {
                 HashMap author = (HashMap) embedded.get("author");
                 HashMap assignee = (HashMap) embedded.get("assignee");
                 HashMap status = (HashMap) embedded.get("status");
+                HashMap type = (HashMap) embedded.get("type");
+                HashMap priority = (HashMap) embedded.get("priority");
                 HashMap project = (HashMap) embedded.get("project");
+                HashMap links = (HashMap) data.get("_links");
+                HashMap description = (HashMap) data.get("description");
+                String desc_html = description.get("html").toString();
+                HashMap customfield6 = (HashMap)  links.get("customField6");
+                HashMap customfield7 = (HashMap) links.get("customField7");
+                String direccion = "";
+                String bien = "";
+                if (customfield6 != null)
+                    direccion = customfield6.get("title").toString();
+                if (customfield7 != null)
+                    bien = customfield7.get("title").toString();
                 String project_name = project.get("identifier").toString();
                 String package_id = data.get("id").toString();
                 List<String> mailto = new ArrayList<String>();
                 String status_name = status.get("name").toString();
+                if (validateEmail(email))
+                    mailto.add(email);
                 mailto.add(author.get("email").toString());
                 if (assignee != null)
                     mailto.add(assignee.get("email").toString());
-                String ticketNo = package_id;
-                this.sendNewMail(mailto.toArray(new String[0]),"Ticket numero: "+ticketNo+" Estado: "+status_name, "Puede ver los detalles en el siguiente enlace: " + serverHost+"projects/"+project_name+"/work_packages/"+package_id+"/activity");
-                logger.info( "Email enviado a:" + mailto);
 
+                String ticketNo = package_id;
+                if (!desc_html.equals("")) {
+                    try {
+                        this.sendNewHTMLmail(mailto.toArray(new String[0]), "Ticket numero: " + ticketNo + " Estado: " + status_name, "Ticket creado: " + type.get("createdAt").toString()
+                                + "<br> Prioridad: " + priority.get("name").toString()
+                                + "<br> Creado por: " + author.get("name").toString()
+                                + "<br> Dirección: " + direccion
+                                + "<br> Id y Desc del bien: " + bien
+                                + "<br> Puede ver los detalles en el siguiente enlace: " + serverHost + "projects/" + project_name + "/work_packages/" + package_id + "/activity"
+                                + desc_html
+                        );
+                    } catch (MessagingException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                else {
+                    this.sendNewMail(mailto.toArray(new String[0]), "Ticket numero: " + ticketNo + " Estado: " + status_name, "Ticket creado: " + type.get("createdAt").toString()
+                            + "\n Prioridad: " + priority.get("name").toString()
+                            + "\n Creado por: " + author.get("name").toString()
+                            + "\n Dirección: " + direccion
+                            + "\n Id y Desc del bien: " + bien
+                            + "\n Puede ver los detalles en el siguiente enlace: " + serverHost + "projects/" + project_name + "/work_packages/" + package_id + "/activity");
+
+                }
+
+
+                logger.info( "Email enviado a:" + mailto);
+                //logger.info(links.toString());
 
             }
             else
@@ -46,9 +90,15 @@ public class sendMail extends EmailService {
         return "Ok\n";
     }
 
-    String trySend (HashMap<String, String> datos, String fromemail){
-        String result = "No se ha podido enviar el correo: ";
-
+    public static boolean validateEmail(String email) {
+        boolean result = true;
+        try {
+            InternetAddress emailAddr = new InternetAddress(email);
+            emailAddr.validate();
+        } catch (AddressException ex) {
+            result = false;
+        }
         return result;
     }
+
 }
